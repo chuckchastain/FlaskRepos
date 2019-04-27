39836d0189510f330636ca526b144e0029f98c55
charles.a.chastain <charles.a.chastain@hpe.com>
2019-04-27 17:40:47 -0400

    WebForms

--- LearningFlask/microblog/AppDir/views.py
+++ LearningFlask/microblog/AppDir/views.py
@@ -1,14 +1,27 @@
-from flask import Flask
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from AppDir import app
-
+from AppDir.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = "Chuck"
-    return render_template(
-      "index.html",
-       title = "Home Page",
-        message = "Hello Flask!",
-        username = user  )
+    user = {'username': 'Chuck'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful Day in Portland!'},
+        {
+             'author': {'username': 'Susan'},
+             'body': 'The Avengers movie was so cool!'}
+    ]
+    return render_template("index.html", 
+              title = 'Home', user = user, posts = posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template("login.html",  title='Sign In', form=form)
