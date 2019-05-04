2984414bd2fb7d48fae55e50742804ac1ecb32f6
charles.a.chastain <charles.a.chastain@hpe.com>
2019-05-04 08:38:33 -0400

    Flask DB changes

--- LearningFlask/microblog/AppDir/__init__.py
+++ LearningFlask/microblog/AppDir/__init__.py
@@ -1,8 +1,12 @@
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from AppDir import views
+from AppDir import views, models
