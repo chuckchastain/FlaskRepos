a9e1e695c1f835190d6a72554f2def77576e309c
charles.a.chastain <charles.a.chastain@hpe.com>
2019-05-27 15:49:23 -0400

    User Logins

--- LearningFlask/microblog/microblog.py
+++ LearningFlask/microblog/microblog.py
@@ -1,12 +1,17 @@
-import os
-from AppDir import app    # Imports the code from AppDir/__init__.py
+#import os
+from AppDir import app, db    # Imports the code from AppDir/__init__.py
+from AppDir.models import User, Post
 
-if __name__ == '__main__':
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
 
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+#if __name__ == '__main__':
+#    HOST = os.environ.get('SERVER_HOST', 'localhost')
 
-    app.run(HOST, PORT)
+ #   try:
+  #      PORT = int(os.environ.get('SERVER_PORT', '5555'))
+  #  except ValueError:
+  #      PORT = 5555
+
+  #  app.run(HOST, PORT)
